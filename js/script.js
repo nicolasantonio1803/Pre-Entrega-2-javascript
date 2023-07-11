49dc9b08b617d16a1e71e5e27b7368c248502f61
Nicolas1803 <nicolavillegas@gmail.com>
2023-07-10 22:28:13 -0300

    ruta img

--- js/script.js
+++ js/script.js
@@ -1,96 +1,48 @@
 let productos = [
-   { id:2, nombre: "Nike Cortez running", categoria: "championes", unidades: 6, precio:800},
-   { id:3, nombre: "Nike Air Fore", categoria: "championes", unidades: 6, precio:700},
-   { id:4, nombre: "Air Jordan 1 Mid ", categoria: "championes", unidades: 6, precio:1200},
-   { id:5, nombre: "Nike Air Max Alpha ", categoria: "championes", unidades: 6, precio:900},
-   { id:6, nombre: "Nike Blezer Mid", categoria: "championes", unidades: 6, precio:1000},
-
- ]
-
- let carrito = []
-
-let mensaje = "Bienvenido a la Tienda\n1 - Lista de Productos  \n2 - Agregar Productos al Carrito  \n3 - Lista de Carrito \n4 - Total a Pagar  \n5 - Para Finalizar Compra\n0 - Salir "
-
-
-let opcion
-
-do {
-   opcion = Number(prompt(mensaje))
-
-   if (opcion === 1) {
-     alert(listar(productos))
-   } else if (opcion === 2) {
-      let id = Number(prompt("Seleccione un id del Producto que quiere Comprar\n" + listar(productos)))
-      let productobuscado = productos.find(prod => prod.id === id)
-      let posicionproductoencarrito = carrito.findIndex(prod => prod.id === productobuscado.id)
-      if (posicionproductoencarrito === -1) {
-         carrito.push({
-            id: productobuscado.id ,
-            nombre: productobuscado.nombre,
-            preciounitario: productobuscado.precio,
-            unidades:1,
-            subtotal: productobuscado.precio
-         })
-      } else{
-         carrito[posicionproductoencarrito].unidades++
-         carrito[posicionproductoencarrito].subtotal = carrito[posicionproductoencarrito].preciounitario * carrito[posicionproductoencarrito].unidades
-      } 
-     console.log(carrito)
-   } if (opcion === 3) {
-      alert(listar(carrito))
-
-   } else if (opcion === 4) {
-      let total = 0
-      carrito.forEach((producto) => {
-        total += producto.subtotal
-      })
-      alert("El total a pagar es: $" + total)
+   { id: 2, nombre: "Nike Cortez running", categoria: "championes", unidades: 6, precio: 6000 },
+   { id: 3, nombre: "Nike Air Fore", categoria: "championes", unidades: 6, precio: 5800 },
+   { id: 4, nombre: "Air Jordan 1 Mid", categoria: "championes", unidades: 6, precio: 5000 },
+   { id: 5, nombre: "Nike Air Max Alpha", categoria: "championes", unidades: 6, precio: 5000 }
+ ];
  
-   } else if (opcion === 5) {
-      let confirmacion = confirm("¿Desea finalizar la compra?")
-      if (confirmacion) {
-        carrito = [];
-        alert("¡Gracias por su compra! Su pedido ha sido procesado.")
-      }
-    }
-   
-}   while (opcion !== 0)
- alert("Muchas gracias por su visita")
-
-function listar(arraylistar) {
-  let listado = "Id - Nombre\n"
-  arraylistar.forEach(element => {
-   listado = listado + element.id + "-" + element.nombre + "\n"
-  })
-  return listado
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ let carritoJSON = JSON.parse(localStorage.getItem("carrito"));
+ let carrito = carritoJSON ? carritoJSON : [];
+ 
+ let contenedor = document.getElementById("productos");
+ creartarjetas(productos, contenedor);
+ renderizarCarrito(carrito);
+ 
+ function creartarjetas(array, contenedor) {
+   contenedor.innerHTML = "";
+   array.forEach((producto) => {
+     let tarjeta = document.createElement("div");
+     tarjeta.className = "contenedorProducto";
+ 
+     tarjeta.innerHTML = `
+       <h3>${producto.nombre}</h3>
+       <img class="img" src="img/${producto.rutaImagen}">
+       <div id=${producto.id} class="button">
+         <div class="button-wrapper">
+           <div class="text">$${producto.precio}</div>
+         </div>
+       </div>`;
+ 
+     contenedor.appendChild(tarjeta);
+     let botonCarrito = document.getElementById(producto.id);
+     botonCarrito.addEventListener("click", agregarCarrito);
+   });
+ }
+ 
+ let buscador = document.getElementById("buscador");
+ buscador.addEventListener("input", filtrar);
+ 
+ function filtrar() {
+   let arrayFiltrado = productos.filter(
+     (producto) =>
+       producto.nombre.toLowerCase().includes(buscador.value.toLowerCase()) ||
+       producto.categoria.toLowerCase().includes(buscador.value.toLowerCase())
+   );
+   creartarjetas(arrayFiltrado, contenedor);
+ }
+ 
+ 
